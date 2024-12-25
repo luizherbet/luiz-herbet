ff256be1eec5d4a66ed9da208e2b57b28dc37946
luizherbetq <viagensdobinho@gmail.com.com>
2024-12-25 16:38:52 -0300

    fix problem

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Main from '../components/Main';
 import Services from '../components/Services';
 import Footer from '../components/Footer';
 import BusinessCard from '../components/Card';
-import NavBarv2 from '../components/NavBar';
 
 const Home = memo(() => {
   return (
