491411759e764d38f464a8831b6f432616dc12ce
luizherbetq <viagensdobinho@gmail.com.com>
2024-12-25 22:26:01 -0300

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,8 @@ import RightMain from './RightMain';
 
 const Main = memo(() => {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-white from-30% to-slate-300 to-10%... h-auto">
-      <div className="flex flex-col sm:flex-row gap-4 p-4 mx-auto w-full sm:w-[900px]">
+    <main className="h-auto bg-gradient-to-b from-white from-30% to-slate-300 to-10%...">
+      <div className="flex flex-col md:flex-row sm:flex-row gap-4 p-4 mx-auto w-full sm:w-[900px]">
         <section className=" text-black p-4 flex-1 h-auto">
           <LeftMain />
         </section>
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,10 +3,10 @@ import Fading from './Fading';
 import {
   FaGlobe,
   FaChartLine,
-  FaShoppingCart,
   FaBrush,
-  FaBullhorn,
-  FaTachometerAlt,
+  FaVideo,
+  FaHeadphones,
+  FaImage,
 } from 'react-icons/fa';
 
 const servicesData = [
@@ -21,24 +21,27 @@ const servicesData = [
     icon: <FaChartLine />,
   },
   {
-    title: 'E-commerce',
-    description: 'Loja virtual personalizada para o seu negócio.',
-    icon: <FaShoppingCart />,
-  },
-  {
     title: 'Branding',
     description: 'Identidade visual para destacar sua marca.',
     icon: <FaBrush />,
   },
   {
-    title: 'Marketing Digital',
-    description: 'Aumente suas vendas com campanhas eficazes.',
-    icon: <FaBullhorn />,
+    title: 'Edição de Áudio',
+    description:
+      'Produção e edição de áudio para podcasts e músicas, com qualidade profissional.',
+    icon: <FaHeadphones />,
   },
   {
-    title: 'Otimização de Performance',
-    description: 'Seu site mais rápido e eficiente.',
-    icon: <FaTachometerAlt />,
+    title: 'Edição de Vídeo',
+    description:
+      'Edição de vídeos para YouTube, Instagram, e outras plataformas digitais.',
+    icon: <FaVideo />,
+  },
+  {
+    title: 'Edição de Imagens',
+    description:
+      'Criação e edição de imagens para thumbnails, posts e materiais promocionais.',
+    icon: <FaImage />,
   },
 ];
 
@@ -46,22 +49,26 @@ const Services = memo(() => {
   return (
     <div
       id="services"
-      className="flex flex-col justify-center items-center bg-gradient-to-b from-slate-300 from-30% to-slate-500 to-10%... min-h-screen"
+      className="flex flex-col justify-center items-center bg-gradient-to-b from-slate-300 from-30% to-slate-500 to-10%... h-auto"
     >
       <Fading time={2000}>
-        <section className=" w-[90%] max-w-[1400px] py-16 ">
+        <section className="w-[90%] max-w-[1400px] py-16">
           <div className="container mx-auto px-6">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
               {servicesData.map((service, index) => (
                 <div
                   key={index}
-                  className="bg-white border border-black rounded-lg shadow-md p-6 flex flex-col items-center justify-center h-[250px] w-[250px] transition duration-300"
+                  className="bg-white border border-black rounded-lg shadow-md p-6 flex flex-col items-center justify-center h-[250px] w-[250px] transition-all duration-300 transform hover:scale-110 hover:bg-indigo-100 hover:text-indigo-600"
                 >
-                  <div className="text-black text-4xl mb-4">{service.icon}</div>
-                  <h3 className="text-lg font-semibold text-black mb-2">
+                  <div className="text-black text-4xl mb-4 transition-colors duration-300 hover:text-indigo-600">
+                    {service.icon}
+                  </div>
+                  <h3 className="text-lg font-semibold text-black mb-2 transition-colors duration-300 hover:text-indigo-600">
                     {service.title}
                   </h3>
-                  <p className="text-gray-800 text-sm">{service.description}</p>
+                  <p className="text-gray-800 text-sm transition-colors duration-300 hover:text-indigo-600">
+                    {service.description}
+                  </p>
                 </div>
               ))}
             </div>
